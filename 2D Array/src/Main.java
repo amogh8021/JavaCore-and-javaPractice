03032f5623e886ce6bc8bfb732524f6c4cf27c3d
Amogh Shrivastav <amoghdev8021@example.com>
2026-08-22 00:58:23 +0530

    initial commit

--- 2D Array/src/Main.java
+++ 2D Array/src/Main.java
@@ -1,6 +1,35 @@
-public class Main{
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
 
-        //i have to take the
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the rows:");
+        int rows = sc.nextInt();
+
+        System.out.println("Enter the cols:");
+        int cols = sc.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        System.out.println("Enter the values:");
+
+        // Taking input
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // Printing matrix
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
     }
 }
